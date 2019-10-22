c5d687095fcd61a7a6e621b58644cea63fa6fb6f
Brehboyy <ousmane16diarra@gmail.com>
2019-10-22 11:56:31 +0200

    [EVO][JDP-12] Creation des etapes du pendu

---
+++ src/DessinPendu.java
@@ -0,0 +1,151 @@
+/**
+ * 
+ * Cette classe va permettre d'afficher au fur et a mesure les differentes etapes du pendu 
+ * 
+ * @version 1.0
+ *
+ * @see DessinPendu
+ * @author Ousmane Diarra
+ */
+public class DessinPendu {
+	/** 
+	 * Dessin des partie du pendu selon l'etape
+	 *  
+	 * @param       etape : c'est le numéro de l'etape en question
+	 *    :           :         : 
+	 * @see afficherEtapesPerdu
+	 * @author   Ousmane Diarra
+	 */
+	public static void afficherEtapesPerdu(int etape) {
+		switch(etape) {
+		case 1:
+			System.out.println("\n");
+			System.out.println("\n");
+			System.out.println("\n");
+			System.out.println("\n");
+			System.out.println("\n");
+			System.out.println("_____________\n");
+			break;
+		case 2:
+			System.out.println("\n");
+			System.out.println(" |\n");
+			System.out.println(" |\n");
+			System.out.println(" |\n");
+			System.out.println(" |\n");
+			System.out.println(" |\n");
+			System.out.println("_|____________\n");
+			break;
+		}
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// 0 //
+
+		System.out.println("\n");
+		System.out.println("\n");
+		System.out.println("\n");
+		System.out.println("\n");
+		System.out.println("\n");
+		System.out.println("\n");
+		System.out.println("\n");
+
+		// 1 //
+
+		System.out.println("\n");
+		System.out.println("\n");
+		System.out.println("\n");
+		System.out.println("\n");
+		System.out.println("\n");
+		System.out.println("_____________\n");
+
+		// 2 //
+
+		System.out.println("\n");
+		System.out.println(" |\n");
+		System.out.println(" |\n");
+		System.out.println(" |\n");
+		System.out.println(" |\n");
+		System.out.println(" |\n");
+		System.out.println("_|____________\n");
+
+		// 3 //
+
+		System.out.println("_____________\n");
+		System.out.println(" |\n");
+		System.out.println(" |\n");
+		System.out.println(" |\n");
+		System.out.println(" |\n");
+		System.out.println(" |\n");
+		System.out.println("_|____________\n");
+		// 4 //
+		System.out.println("_____________\n");
+		System.out.println(" | /\n");
+		System.out.println(" |/\n");
+		System.out.println(" |\n");
+		System.out.println(" |\n");
+		System.out.println(" |\n");
+		System.out.println("_|____________\n");
+
+		// 5 //
+
+		System.out.println("_____________\n");
+		System.out.println(" | /       |\n");
+		System.out.println(" |/\n");
+		System.out.println(" |\n");
+		System.out.println(" |\n");
+		System.out.println(" |\n");
+		System.out.println("_|____________\n");
+
+		// 6 //
+
+		System.out.println("_____________\n");
+		System.out.println(" | /       |\n");
+		System.out.println(" |/        O\n");
+		System.out.println(" |\n");
+		System.out.println(" |\n");
+		System.out.println(" |\n");
+		System.out.println("_|____________\n");
+
+		// 7 //
+		System.out.println("_____________\n");
+		System.out.println(" | /       |\n");
+		System.out.println(" |/        O\n");
+		System.out.println(" |         |\n");
+		System.out.println(" |\n");
+		System.out.println(" |\n");
+		System.out.println("_|____________\n");
+
+		// 8 //
+
+		System.out.println("_____________\n");
+		System.out.println(" | /       |\n");
+		System.out.println(" |/        O\n");
+		System.out.println(" |        -|-\n");
+		System.out.println(" |\n");
+		System.out.println(" |\n");
+		System.out.println("_|____________\n");
+
+		// 9 //
+
+		System.out.println("_____________\n");
+		System.out.println(" | /       |\n");
+		System.out.println(" |/        O\n");
+		System.out.println(" |        -|-\n");
+		System.out.println(" |         /\\\n");
+		System.out.println(" |\n");
+		System.out.println("_|____________\n");
+
+		// 10 //
+
+		System.out.println("_____________\n");
+		System.out.println(" | /       |\n");
+		System.out.println(" |/        O\n");
+		System.out.println(" |        -|-\n");
+		System.out.println(" |         /\\\n");
+		System.out.println(" |\n");
+		System.out.println("_|____________\n");
+		System.out.println("Trop tard je suis mort !\n");
+	}
+
+}
