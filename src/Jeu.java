0bfeea1b091206c499c2dce06be1f2f3d256d07a
cara 125 <veron.mael@orange.fr>
2019-10-22 15:44:50 +0200

    [EVO][JDP-23]Algorithme de vérification de la bonne lettre

---
+++ src/Jeu.java
@@ -0,0 +1,75 @@
+import com.sun.xml.internal.ws.util.StringUtils;
+
+public class Jeu {
+
+	Menu menu = new Menu();	
+	
+	/** 
+	 * Lance le jeu
+	 *  
+	 * @see LancerJeuy
+	 * @author Mael Veron
+	 */
+	public void LancerJeu() 
+	{	
+		int dif = menu.accueil();
+		
+		String mot = "";
+		
+		if(dif == 1) 
+		{
+			System.out.println("Vous êtes en mode débutant");
+			//mot =
+		}
+		else if (dif == 2)
+		{
+			System.out.println("Vous êtes en mode avancé");
+			//mot =
+		}
+		
+		mot ="lettre";
+		VerificationLettre(mot);
+	
+	}
+	
+	/** 
+	 * Vérifie les lettres dans le mot
+	 *  
+	 * @see VerificationLettre
+	 * @author Mael Veron
+	 * @param String mot le mot généré
+	 */
+	public void VerificationLettre(String mot) 
+	{
+		int essai = 0;
+		boolean motBon = false;
+		int nbLettreMot = mot.length();
+		int nbLettreBonne = 0;
+		System.out.println(nbLettreMot);
+		while( motBon == false && essai < 10)
+		{
+			String lettre = menu.choixLettre();
+			if(mot.contains(lettre)) {
+				
+				System.out.println("Bonne lettre nb " +  mot.chars().filter(ch -> ch == lettre.charAt(0)).count());
+				nbLettreBonne += mot.chars().filter(ch -> ch == lettre.charAt(0)).count();
+				System.out.println(nbLettreBonne);				
+				if(nbLettreBonne >= nbLettreMot) {
+					motBon = true;
+				}
+			}
+			else
+			{
+				System.out.println("Mauvaise lettre");
+			}
+			
+			essai++;
+		}
+		
+		if(motBon == true) {
+			System.out.println("Vous avez trouver le bon mot, félicitations !!");
+		}else {
+			System.out.println("Vous vous êtes trompé, dommage...");
+		}
+	}
+}
