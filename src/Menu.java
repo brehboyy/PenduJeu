74d373f73ecc933147505d2ba2bf13abb6501b2f
cara 125 <veron.mael@orange.fr>
2019-10-22 14:36:44 +0200

    [EVO][JDP-16] Mise en place menu

---
+++ src/Menu.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class Menu {
+	
+	private Scanner console = new Scanner(System.in);
+	
+	public Menu() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/** 
+	 * Accueil du programme
+	 *  
+	 * @see accueil
+	 * @author Mael Veron
+	 * @return int le niveau de difficulte
+	 */
+	public int accueil() {		
+		
+		System.out.println("Bonjour et bienvenue sur le jeu du pendu de la Suicide Squad");
+		System.out.println("Veuillez sélectionnez un niveau de difficulté entre 1 (débutant) et 2 (avancé) :");
+        int number = console.nextInt();
+        return number;
+	}
+	
+	/** 
+	 * Affiche la phrase de choix d'une lettre
+	 *  
+	 * @see choixLettre
+	 * @author Mael Veron
+	 * @return String la lettre choisit
+	 */
+	public String choixLettre() {
+		System.out.println("Veuillez choisir une lettre");
+		String lettre = console.nextLine();
+		return lettre;
+	}
+	
+	
+}
