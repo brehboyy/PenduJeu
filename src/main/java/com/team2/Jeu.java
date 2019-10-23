faf01a8af01bda094f32ec536eee6087aa79323a
Brehboyy <ousmane16diarra@gmail.com>
2019-10-23 13:36:27 +0200

    [ANO] Mettre tous les contrôles pour le programme ne plante pas

--- src/main/java/com/team2/Jeu.java
+++ src/main/java/com/team2/Jeu.java
@@ -4,101 +4,119 @@ import java.util.Arrays;
 
 public class Jeu {
 
-	Menu menu = new Menu();	
-	
-	/** 
+	Menu menu = new Menu();
+
+	/**
 	 * Lance le jeu
-	 *  
+	 * 
 	 * @see LancerJeu
 	 * @author Mael Veron
 	 */
-	public void lancerJeu() 
-	{	
+	public void lancerJeu() {
 		int dif = menu.accueil();
-		
+
 		String mot = "";
 		DictionnaireDeMot dic = new DictionnaireDeMot("test.txt");
-		if(dif == 1) 
-		{
+		if (dif == 1) {
 			System.out.println("Vous etes en mode debutant\n");
 			mot = dic.getMotFacile();
-		}
-		else if (dif == 2)
-		{
+		} else if (dif == 2) {
 			System.out.println("Vous etes en mode avance\n");
 			mot = dic.getMotDur();
 		}
-		
-		
-		if(verificationLettre(mot) == true) {
+		System.out.println("le mot contient " + mot.length() + " lettres");
+
+		if (verificationLettre(mot) == true) {
 			System.out.println("Vous avez trouver le bon mot, felicitations !!");
-		}else {
+		} else {
 			System.out.println("Vous vous etes trompe, dommage...");
 		}
-		
+
 		System.out.println("Le mot etait " + mot);
 	}
-	
-	/** 
+
+	/**
 	 * Verifie les lettres dans le mot
-	 *  
+	 * 
 	 * @see VerificationLettre
 	 * @author Mael Veron
 	 * @param String mot le mot genere
 	 * @return reussite
 	 */
-	public boolean verificationLettre(String mot) 
-	{
+	public boolean verificationLettre(String mot) {
 		char[] motLettre = mot.toCharArray();
 		char[] lettreAtrouver = new char[motLettre.length];
-		
-		for(int i = 0; i < motLettre.length; i++) {
+
+		for (int i = 0; i < motLettre.length; i++) {
 			lettreAtrouver[i] = '_';
 		}
-	
+
 		int essai = 0;
 		boolean reussite = false;
 		boolean temp = false;
 		affichage(lettreAtrouver);
-		
-		while(reussite == false && essai < 10)
-		{
+
+		while (reussite == false && essai < 10) {
 			char lettre = menu.choixLettre().charAt(0);
-			for(int i = 0; i < motLettre.length; i++)
-			{
-				temp = false;
-				if(motLettre[i] == lettre) 
-				{
-					lettreAtrouver[i] = lettre;						
+			if (estDansMot(lettre, motLettre)) {
+				for (int i = 0; i < motLettre.length; i++) {
+					if (motLettre[i] == lettre) {
+						lettreAtrouver[i] = lettre;
+					}
 				}
-				
-				else if(lettreAtrouver[i] == '_') 
-				{
-					temp = true;
-				}
+			}else {
+				essai++;
+				DessinPendu.afficherEtapesPerdu(essai);
 			}
-			if(temp == false) 
-			{
-				reussite = true;
-			}
-			
+			reussite = estComplet(lettreAtrouver);
 			affichage(lettreAtrouver);
-			essai++;			
-		}		
+		}
 		return reussite;
 	}
 	
-	/** 
+	
+	/**
+	 * Verifie si on a trouver le mot completement ou si il reste encore des mots a touver
+	 * 
+	 * @see estComplet
+	 * @author Ousmane Diarra
+	 * @param char[] un tableau de caractere
+	 * @return trouve
+	 */
+	public boolean estComplet(char[] LettresTrouve) {
+		boolean trouve = true;
+		for(char c : LettresTrouve) {
+			if(c == '_') {
+				trouve = false;
+			}
+		}
+		return trouve;
+	}
+	
+	
+	/**
+	 * Verifie Verifie si une lettre est dans un mot
+	 * 
+	 * @see estDansMot
+	 * @author Ousmane Diarra
+	 * @param char lettre a verifier
+	 * @param char[] un tableau de caractere
+	 * @return boolean
+	 */
+	public boolean estDansMot(char lettre, char[] motTab) {
+		return (new String(motTab)).contains(lettre + "");
+	}
+
+	/**
 	 * Affiche les lettres
-	 *  
+	 * 
 	 * @see affichage
 	 * @author Mael Veron
 	 * @param char[] lettreAtrouver
 	 */
 	public void affichage(char[] lettreAtrouver) {
-		String affichage ="";
-		for(int i = 0; i < lettreAtrouver.length; i++) 
-		{
+		String affichage = "";
+		for (int i = 0; i < lettreAtrouver.length; i++) {
 			affichage += lettreAtrouver[i] + " ";
 		}
 		System.out.println(affichage);
