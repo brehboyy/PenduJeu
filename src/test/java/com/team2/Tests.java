d2f441912e82971d87f50d1ef16619069bd77814
Brehboyy <ousmane16diarra@gmail.com>
2019-10-23 16:18:36 +0200

    [EVO] [SP12] Continuité

--- src/test/java/com/team2/Tests.java
+++ src/test/java/com/team2/Tests.java
@@ -17,41 +17,41 @@ import org.junit.Test;
 public class Tests {
 
 
-	@Test
-	public void testGetMotFacile() {
-		FichierDictionnaireDeMot ddm = new FichierDictionnaireDeMot();
-		ddm.RemplirListe("test.txt");
-		assertEquals(ddm.getMotFacile(), "foch");
-	}
+    @Test
+    public void testGetMotFacile() {
+        FichierDictionnaireDeMot ddm = new FichierDictionnaireDeMot();
+        ddm.RemplirListe("test.txt");
+        assertEquals(ddm.getMotFacile(), "foch");
+    }
 
-	@Test
-	public void testGetMotDur() {
-		FichierDictionnaireDeMot ddm = new FichierDictionnaireDeMot();
-		ddm.RemplirListe("test.txt");
-		assertEquals(ddm.getMotDur(), "publication");
-	}
-	
-	@Test
-	public void testequalTabTrue() {
-		Jeu jeu = new Jeu();
-		assertEquals(jeu.estComplet(new char[] {'a', 'b',  'c'}), true);
-	}
-	
-	@Test
-	public void testequalTabFalse() {
-		Jeu jeu = new Jeu();
-		assertEquals(jeu.estComplet(new char[] {'a', 'b',  '_'}), false);
-	}
-	
-	@Test
-	public void testestDansMotTrue() {
-		Jeu jeu = new Jeu();
-		assertEquals(jeu.estDansMot('c',new char[] {'a', 'b',  'c'}), true);
-	}
-	
-	@Test
-	public void testestDansMotFalse() {
-		Jeu jeu = new Jeu();
-		assertEquals(jeu.estDansMot('z',new char[] {'a', 'b',  'c'}), false);
-	}
+    @Test
+    public void testGetMotDur() {
+        FichierDictionnaireDeMot ddm = new FichierDictionnaireDeMot();
+        ddm.RemplirListe("test.txt");
+        assertEquals(ddm.getMotDur(), "publication");
+    }
+
+    @Test
+    public void testequalTabTrue() {
+        Jeu jeu = new Jeu();
+        assertEquals(jeu.estComplet(new char[] {'a', 'b',  'c'}), true);
+    }
+
+    @Test
+    public void testequalTabFalse() {
+        Jeu jeu = new Jeu();
+        assertEquals(jeu.estComplet(new char[] {'a', 'b',  '_'}), false);
+    }
+
+    @Test
+    public void testestDansMotTrue() {
+        Jeu jeu = new Jeu();
+        assertEquals(jeu.estDansMot('c',new char[] {'a', 'b',  'c'}), true);
+    }
+
+    @Test
+    public void testestDansMotFalse() {
+        Jeu jeu = new Jeu();
+        assertEquals(jeu.estDansMot('z',new char[] {'a', 'b',  'c'}), false);
+    }
 }
